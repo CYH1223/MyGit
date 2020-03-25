f9e626c83c7715932dca44d903669ef4872053b6
CYH1223“git config --global user.nemail ”18839136131@163..com <18839136131@163.com>
2020-03-25 17:35:50 +0800

    first commit

---
+++ my-generate/src/main/java/com/qf/portal/dao/HotelMapper.java
@@ -0,0 +1,22 @@
+package com.qf.portal.dao;
+
+import com.qf.portal.pojo.po.Hotel;
+import com.qf.portal.pojo.po.HotelExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface HotelMapper {
+    int countByExample(HotelExample example);
+
+    int deleteByExample(HotelExample example);
+
+    int insert(Hotel record);
+
+    int insertSelective(Hotel record);
+
+    List<Hotel> selectByExample(HotelExample example);
+
+    int updateByExampleSelective(@Param("record") Hotel record, @Param("example") HotelExample example);
+
+    int updateByExample(@Param("record") Hotel record, @Param("example") HotelExample example);
+}
